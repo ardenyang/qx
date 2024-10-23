ebad8ed40d04a2578a0eaf71925a17da29d12ea7
ardenyang <63284262+ardenyang@users.noreply.github.com>
2024-10-23 13:17:22 +0800

    Update and rename lebo_noads to lebo_noads.js

--- lebo_noads.js
+++ lebo_noads.js
@@ -4,4 +4,4 @@ https:\/\/saas\.hpplay\.cn\/api\/lebo-desk\/endpoint\/app-resource\/app_home_tip
 https:\/\/saas\.hpplay\.cn\/api\/lebo-desk\/endpoint\/app-resource\/app_index_operate2 url reject-dict
 https:\/\/saas\.hpplay\.cn\/api\/lebo-rabbit\/app-card\/list url reject-dict
 https:\/\/saas\.hpplay\.cn\/api\/lebo-desk\/endpoint\/app-resource\/app-user-banner url reject-dict
-https:\/\/saas\.hpplay\.cn\/api\/lebo-desk\/endpoint\/app-resource\/app_tab_red_dot\?user-tag=1$ url script-response-body https://gitlab.com/lodepuly/vpn_tool/-/raw/master/Resource/Script/TV_Assistant/TV_Assistant_remove_ads.js
+https:\/\/saas\.hpplay\.cn\/api\/lebo-desk\/endpoint\/app-resource\/app_tab_red_dot\?user-tag=1$ url script-response-body https://raw.githubusercontent.com/ardenyang/qx/main/lebo_noads_body.js
