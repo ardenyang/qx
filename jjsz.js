46acf9666c6d41c7c6cb5e1d7248d7f6b94d2e89
ardenyang <63284262+ardenyang@users.noreply.github.com>
2025-01-25 22:10:35 +0800

    更新 jjsz.js

--- jjsz.js
+++ jjsz.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/jjsz.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/jjsz.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/ardenyang/qx/main/jjsz.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/ardenyang/qx/main/jjsz.js
 
 [mitm]
 hostname = api.revenuecat.com
